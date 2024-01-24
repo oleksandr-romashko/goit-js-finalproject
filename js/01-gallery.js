e3cdbd9acbca982d44cb7446abfe1c7fe3b3413e
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-01-24 23:34:25 +0100

    WIP Add task-1 redner and delegation

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+gallery.insertAdjacentHTML("afterbegin", createMarkup(galleryItems));
+
+gallery.addEventListener("click", handleImageClick);
+
+/**
+ * * Generates markup for gallery items.
+ * @param {object[]} arr Array of gallery objects.
+ * @returns {string} Gallery items markup in text format.
+ */
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </li>`
+    )
+    .join("");
+}
+
+function handleImageClick(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  const gallery = event.target.closest(".gallery");
+  const originalImg = event.target.dataset.source;
+  const description = event.target.alt;
+
+  console.log(gallery);
+  console.log(originalImg);
+  console.log(description);
+}
